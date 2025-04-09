bd4e3611b1dfcdcb18aded84a8597c2086808252
tqphu27 <trn.quangph27@gmail.com>
2025-04-09 17:11:38 +0700

    9091

--- main.py
+++ main.py
@@ -142,7 +142,7 @@ if __name__ == "__main__":
     uvicorn.run(
         app,
         host="0.0.0.0",
-        port=9090,
+        port=9091,
         ssl_keyfile="ssl/key.pem",
         ssl_certfile="ssl/cert.pem"
     )
